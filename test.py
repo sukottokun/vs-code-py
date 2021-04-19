85c90c06ba457e7318f8c0936590f571a3e20eba
Scott Massey <scott.massey@gmail.com>
2021-04-19 11:03:41 +0900

    Moved some code

--- test.py
+++ test.py
@@ -1,15 +1,4 @@
-import math
-import sys
-
 import requests
-
-# prclearint(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("https://prolificsys.com")
